6265ebbf6107ce33b8d0a316cd0dd0eb68d765c9
oluwafemi <oluwafemidojumo@gmail.com>
2025-09-05 19:34:21 +0100

    redis_cache/hit/miss_metrics

--- properties/utils.py
+++ properties/utils.py
@@ -4,7 +4,6 @@ import logging
 from django_redis import get_redis_connection
 
 
-
 def get_all_properties():
     properties = cache.get('all_properties')
     if properties is None:
@@ -19,19 +18,28 @@ logger = logging.getLogger(__name__)
 
 
 def get_redis_cache_metrics():
-    redis_con = get_redis_connection('defualt')
-    info = redis_con.info
+    try:
+        redis_con = get_redis_connection('defualt')
+        info = redis_con.info
 
-    hits = info.get('keyspace_hits', 0)
-    misses = info.get('keyspace_misses', 0)
-    total = hits + misses
-    hit_ratio = (hits / total)
+        hits = info.get('keyspace_hits', 0)
+        misses = info.get('keyspace_misses', 0)
+        total_requests = hits + misses
+        hit_ratio = (hits / total_requests) if total_requests > 0 else 0
 
-    metrics = {
-        "keyspace_hits": hits,
-        "keyspace_misses": misses,
-        "hit_ratio": round(hit_ratio, 4) if hit_ratio is not None else "N/A"
-    }
+        metrics = {
+            "keyspace_hits": hits,
+            "keyspace_misses": misses,
+            "hit_ratio": round(hit_ratio, 4) if hit_ratio is not None else "N/A"
+        }
 
-    logger.info(f"Redis Cache Metrics: {metrics}")
-    return metrics
+        logger.info(f"Redis Cache Metrics: {metrics}")
+        return metrics
+
+    except Exception as e:
+        logger.error(f"Failed to retrieve Redis metrics: {e}")
+        return {
+            "keyspace_hits": 0,
+            "keyspace_misses": 0,
+            "hit_ratio": 0
+        }
